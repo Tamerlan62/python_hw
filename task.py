bddab6b41fa0164f70856f6baee61b1961101b4e
Tamerlan62 <tamerlan.recebov062@gmail.com>
2024-07-31 16:48:19 +0400

    creat code

---
+++ task.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import random
+
+
+lst = ['robot'] * 10
+lst += ['human'] * 10
+random.shuffle(lst)
+data = pd.DataFrame({'whoAmI': lst})
+
+
+data['is_robot'] = data['whoAmI'].apply(lambda x: 1 if x == 'robot' else 0)
+data['is_human'] = data['whoAmI'].apply(lambda x: 1 if x == 'human' else 0)
+
+
+print(data.head())
